3c0b257b679f44a9f08fab112dc509761b13fe72
Rafael Duarte <rfd.duarte@gmail.com>
2021-12-20 22:25:09 -0300

    Sistema Alunos

---
+++ sistema.js
@@ -0,0 +1,36 @@
+let nomeDeAluno = ["Marcel", "Eduardo", "Julia", "Rafaela"]
+for (let nome of nomeDeAluno) {
+    console.log(`Este aluno(a) se chama ${nome}`)
+}
+
+// let numeroDeAlunos = 10;
+
+// let contador = 0;
+
+// while(contador<=numeroDeAlunos){
+
+//     if(contador==0){
+//         console.log("O número atual é zero")
+//     } else if(contador%2!=0){
+//         console.log("O número " + contador + " é ímpar")
+//     } else{
+//         console.log(`O número ${contador} é par`)
+//     }
+//     contador++;
+
+// }
+
+// let numeroDeAlunos = 10;
+// for (let contador = 0; contador <= numeroDeAlunos; contador++) {
+
+//     if(contador==0){
+//         console.log("O número atual é zero")
+//     } else if(contador%2==0){
+//         console.log("O número " + contador + " é par")
+//     } else{
+//         console.log(`O número ${contador} é ímpar`)
+//     }
+    
+        
+//     }
+// }
